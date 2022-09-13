68c4fe6451c5e98970d91c12f461be17307beafa
Duane David <duanedave17@gmail.com>
2022-09-13 01:39:01 -0700

    Refactor app component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-/* eslint-disable */
-
 import React from 'react';
 import Calculator from './components/calculator/Calculator';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => {
+  <div className="App">
+    <Calculator />
+  </div>;
+};
 
 export default App;
