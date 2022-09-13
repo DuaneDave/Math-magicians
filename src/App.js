8bcd21448643b4193b11377b520e351a7ca3804a
Duane David <duanedave17@gmail.com>
2022-09-13 01:32:02 -0700

    Install router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 import React from 'react';
-import Calculator from './components/Calculator';
+import Calculator from './components/calculator/Calculator';
 
 class App extends React.Component {
   render() {
