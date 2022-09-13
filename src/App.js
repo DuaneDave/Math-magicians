7106fdf56b360a80d1aeaa94f27a52f5a604336b
Duane David <duanedave17@gmail.com>
2022-09-13 07:05:35 -0700

    Update index.js to use router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import Calculator from './components/calculator/Calculator';
+import { Routes, Route } from 'react-router-dom';
 
-const App = () => {
-  <div className="App">
-    <Calculator />
-  </div>;
-};
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import Quote from './components/quote/Quote';
+import Calculate from './components/calculator/Calculate';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quote" element={<Quote />} />
+        <Route path="/calculate" element={<Calculate />} />
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
